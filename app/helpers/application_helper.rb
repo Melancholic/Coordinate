991c66d261b9ccf9d61a7a65a3a69cc51a6295f3
Andrey <grizzly-terror@yandex.ru>
2015-03-01 13:18:14 +0400

    add static pages red tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def app_name
     Rails.application.class.parent_name
   end
+
+  def full_title(page_title)
+    if (page_title.empty?)
+      app_name
+    else
+      "#{page_title} | #{app_name}"
+    end
+  end
 end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe "StaticPages" do
+  subject{ page }
+  describe "Home page" do
+    before do
+      visit root_path
+    end
+    it { should have_title(full_title(''))}
+  end
+  describe "FAQ page" do
+  before { visit faq_path }
+    it {should have_content("FAQ") }
+    it {should have_title(full_title("FAQ")) }
+  end 
+  describe "About page" do
+  before { visit about_path }
+    it {should have_title(full_title("About")) }
+  end 
+  describe "Contacts page" do
+  before { visit contacts_path }
+    it {should have_selector('h1', text: 'to contact me') }
+    it {should have_title(full_title("Contacts"))}
+  end 
+
+  it "should have the right links on the Layout" do
+    visit root_path;
+    should_not have_link('Home',root_path);
+    click_link("About");
+    expect(page).to have_title(full_title('About'));
+    should have_link('Home',root_path);
+    click_link("Home");   
+    expect(page).to have_title(full_title(''));
+    click_link("Sign Up");
+    expect(page).to have_title(full_title('Registration'));
+    click_link("Contacts");
+    expect(page).to have_title(full_title('Contacts'));
+
+  end
+end
+

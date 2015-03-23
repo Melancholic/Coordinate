e7085fae095dc7a2a8532550b90ee2bdbc6ca197
Andrey <grizzly-terror@yandex.ru>
2015-03-23 21:42:28 +0400

    Add map by MapBox on root page for signed users. Showes all tracks this users (for test)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,21 @@
 class StaticPagesController < ApplicationController
+
   def home
-  #  @location= Location.new(address: 'Stupino');
-  #  @location.geocode;
-  #  @marks=[];
+    if signed_in?
+      @locs=[];
+     # if(params.has_key?(:start) && params.has_key?(:end))
+     #   start=params[:start].to_date;
+     #   stop=params[:end].to_date;
+     #   @locs=Track.find(:all, :conditions => {:start_time => start..stop, :stop_time => start..stop})
+      current_user.cars.first.tracks.each{|x| @locs+=x.track_locations} if signed_in?;
+     #end
+    end
+    @user_mark=[request.location.latitude, request.location.longitude];
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
   end
 
   def about

fe9bb7a814ec98a964b38d1682af9ffc70b54bd0
Andrey <grizzly-terror@yandex.ru>
2015-03-31 20:12:09 +0400

    Add datepiker in filters  for tracks

--- app/controllers/maps/tracks_controller.rb
+++ app/controllers/maps/tracks_controller.rb
@@ -3,6 +3,19 @@ class Maps::TracksController <  Maps::JsonController
 	before_filter :correct_param
 	def index
 		@car=Car.find_by_id(params[:car]);
-		render status: 200, :json => { :success => true, tracks: @car.tracks , :info => "ok!"}
+
+		time_start=(DateTime.parse(params[:start_date]).utc) if params.has_key?(:start_date);
+		time_end=(DateTime.parse(params[:end_date]).utc)if params.has_key?(:end_date);
+		time_end=time_end.change({ hour: 23, min: 59, sec: 59 }) if time_end;
+		if(time_start &&  time_end)
+			@tracks=@car.tracks.where(start_time: time_start..time_end);
+		elsif(time_start)
+			@tracks=@car.tracks.where(['start_time >= ?', time_start]);
+		elsif(time_end)
+			@tracks=@car.tracks.where(['start_time <= ?', time_end]);
+		else
+			@tracks=@car.tracks;
+		end
+		render status: 200, :json => { :success => true, tracks: @tracks , :info => "ok!"}
 	end
 end

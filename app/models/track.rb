8855e18018af00bc2c1f665e39ce8a25403c86ee
Andrey <grizzly-terror@yandex.ru>
2015-04-02 19:17:10 +0400

    Fix order for track_locations associations

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,6 @@
 class Track < ActiveRecord::Base
 	belongs_to :car;
-	has_many :track_locations, dependent: :destroy;
+	has_many :track_locations, -> { order(time: :asc) }, dependent: :destroy ;
 	default_scope -> { order(:stop_time => :desc) }
 	def create_location(args)
 		self.track_locations.create(args)

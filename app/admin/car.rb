67dae420631e8ec105db5dd641ac71435b518899
Andrey <grizzly-terror@yandex.ru>
2015-04-13 22:19:01 +0300

    Change admnin resource for car

--- app/admin/car.rb
+++ app/admin/car.rb
@@ -1,11 +1,22 @@
 ActiveAdmin.register Car do
     menu priority: 3
   permit_params do 
-    permitted=[:id,:title, :description, :uuid, :user_id];
+    permitted=[:id,:title, :description, :uuid, :user_id, :color, :priority];
     permitted
   end
 
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+    f.inputs "Car Details" do
+      f.input :user
+      f.input :title
+      f.input :description
+      f.input :color, as: :select, :collection =>Color.all, label: "Color"
 
+      f.input :priority
+    end
+    f.actions                                        
+  end
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #

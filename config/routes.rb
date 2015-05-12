c9974b4c2f95b3b873353875e612f79262440e74
Andrey <grizzly-terror@yandex.ru>
2015-05-12 22:12:06 +0300

    Disable index action for Users

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy];
   match '/signin', to: 'sessions#new', via:'get';
   match '/signout', to:'sessions#destroy', via:'delete';
-  resources :users do
+  resources :users, except: [:index] do
     
      #add user/id/following and user/id/followers
     member do

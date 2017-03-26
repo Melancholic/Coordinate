ae68d3e7b66008b7c291a9c12b7cb481f2e196ac
Melancholic <grizzly-terror@yandex.ru>
2017-03-26 06:20:47 -0400

    Configure log queries

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,8 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+  
+  #config.active_record.logger = Logger.new("#{Rails.root}/log/queries.log");
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false

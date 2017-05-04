2c736b07a803bad1333e2da7a0aac864253ce1cb
nagorny <mail@anagorny.com>
2017-05-04 11:35:05 +0300

    Create location and track in one transaction

--- app/controllers/api/v1/geodata_controller.rb
+++ app/controllers/api/v1/geodata_controller.rb
@@ -1,32 +1,34 @@
 class Api::V1::GeodataController < Api::V1::BaseController
-	before_action :check_token
-	before_action :check_data
-	protect_from_forgery unless: -> { request.format.json? }
+  before_action :check_token
+  before_action :check_data
+  protect_from_forgery unless: -> {request.format.json?}
 
-	# CURL test 
-	# curl  -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token <Token>" -X POST -d '{"longitude": "0", "latitude": "0", "speed":1, "time":'$(date "+%s%3N")'}'  localhost:3000/api/v1/geodata/recive
-	def recive
-		new_loc_time = convert_time(params[:time]);
-		@car = Car.find(ApiToken.find_by(token: get_token()).car_id)
-		@cur_track = find_track(@car, params);
-		begin
-			@cur_track.create_location!({longitude:params[:longitude],latitude:params[:latitude],	speed:params[:speed]*3.6 ,accuracy:params[:accuracy], time:convert_time(params[:time])});
-			logger.debug("Location has been saved.")
-			render status: 200, :json => { :success => true, :info => "New location saved.", clear: true} 
-		rescue ActiveRecord::RecordNotUnique => x
-			logger.info("Location alredy exist.");
-			logger.error(x.to_s)
-			render status: 200, :json => { :success => true, :info => "Location alredy exist.", clear: true}
-		end
+  # CURL test
+  # curl  -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token <Token>" -X POST -d '{"longitude": "0", "latitude": "0", "speed":1, "time":'$(date "+%s%3N")'}'  localhost:3000/api/v1/geodata/recive
+  def recive
+    new_loc_time = convert_time(params[:time])
+    ActiveRecord::Base.transaction do
+      @car = Car.find(ApiToken.find_by(token: get_token()).car_id)
+      @cur_track = find_track(@car, params)
+      begin
+        @cur_track.create_location!({longitude: params[:longitude], latitude: params[:latitude], speed: params[:speed]*3.6, accuracy: params[:accuracy], time: convert_time(params[:time])});
+        logger.debug("Location has been saved.")
+        render status: 200, :json => {:success => true, :info => "New location saved.", clear: true}
+      rescue ActiveRecord::RecordNotUnique => x
+        logger.info("Location alredy exist.")
+        logger.error(x.to_s)
+        render status: 200, :json => {:success => true, :info => "Location alredy exist.", clear: true}
+      end
+    end
 
-	end
+  end
 
 
-private
+  private
 
   def find_track(car, params={})
     time = convert_time(params[:time])
-    result_track = Track.new(car: car);
+    result_track = Track.new(car: car)
     potential_tracks = TrackLocation.unscoped.by_car(car)
                            .where(time: time - Track::TIME_INTERVAL .. time + Track::TIME_INTERVAL)
                            .group(:track_id).minimum(:time).keys
@@ -46,20 +48,20 @@ private
       result_track.save
     end
     return result_track
-	end
-	
+  end
 
-	def convert_time(ms)
-		#Time.at(1427021993900/1000).utc
-		return Time.at(ms/1000).utc
-	end
+
+  def convert_time(ms)
+    #Time.at(1427021993900/1000).utc
+    return Time.at(ms/1000).utc
+  end
 
   def check_data
-		unless  (params.has_key?(:longitude)&& params.has_key?(:latitude)&& params.has_key?(:speed) && params.has_key?(:time))
-			render status: :bad_request, :json => { :success => false, :info => "Required parameters not present: longitude, latitude, speed, time", clear: true} 
-		end
-		if (params[:speed].to_f == 0)
-			render status: :bad_request, :json => { :success => false, :info => "Speed can't be zero!", clear: true} 
-		end
-	end
+    unless (params.has_key?(:longitude)&& params.has_key?(:latitude)&& params.has_key?(:speed) && params.has_key?(:time))
+      render status: :bad_request, :json => {:success => false, :info => "Required parameters not present: longitude, latitude, speed, time", clear: true}
+    end
+    if (params[:speed].to_f == 0)
+      render status: :bad_request, :json => {:success => false, :info => "Speed can't be zero!", clear: true}
+    end
+  end
 end

96eede829e9030a3a1753708e4d6aac6c548546f
nagorny <mail@anagorny.com>
2017-05-04 10:02:35 +0300

    Fix trouble, where  geocoder is down

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,14 +3,14 @@ class Location < ApplicationRecord
   validates :time, presence: false
   validates :distance, presence: false
   reverse_geocoded_by :latitude, :longitude
-  after_validation :geocode_run;
+  after_validation :geocode_run
 
 
   private
   def geocode_run
     begin
       reverse_geocode
-    rescue Errno::ENETUNREACH => x
+    rescue Errno::ENETUNREACH, Geocoder::NetworkError => x
       logger.error("Geocoder is down: #{x.to_s}")
     end
   end
